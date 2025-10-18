5ab6a215b214f6087c9f13b314b07b1f63838332
023000722-bit <023000722@upsrj.edu.mx>
2025-10-18 00:08:00 +0000

    Actualización de archivos y adición del servicio de compras

--- src/purchases_service/api.py
+++ src/purchases_service/api.py
@@ -11,14 +11,14 @@ import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask, jsonify
 from common.utils import load_item, get_host
-from common.vars import USERS_FILE, USER_API_URL
+from common.vars import PRODUCTS_FILE, PURCHASES_API_URL
 
 app = Flask(__name__)
 
-@app.route('/api/users', methods=['GET'])
-def get_users():
-    users = load_item(USERS_FILE)
-    return jsonify(users)
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = load_item(PRODUCTS_FILE)
+    return jsonify(products)
 
 if __name__ == '__main__':
-    app.run(port=get_host(USER_API_URL))
+    app.run(port=get_host(PURCHASES_API_URL))
